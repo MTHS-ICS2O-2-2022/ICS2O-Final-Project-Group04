10a525e915b8d4e5739e2f43cd17176d9d6a3a9c
william <124389603+william-simard@users.noreply.github.com>
2023-06-09 17:14:17 +0000

    game.js and flow

---
+++ js/game.js
@@ -0,0 +1,13 @@
+/*global Phaser */
+
+
+// Copyright (c) 2023 william simard All rights reserved
+//
+// Created by: william simard
+// Created on: may 2023
+// This file contains the JS functions for index.html
+
+// scene import statments
+
+import tilescreen from "./js/tilescreen.js";
+import splashscreen from "./js/splashscreen.js";  
