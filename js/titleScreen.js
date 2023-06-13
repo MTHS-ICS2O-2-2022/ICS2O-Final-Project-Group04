d5c9b20b7055d6ffbf4cbde3686f140fc1e4a0aa
Colton-Grant <124389241+Colton-Grant@users.noreply.github.com>
2023-06-13 17:20:46 +0000

    added css

--- js/titleScreen.js
+++ js/titleScreen.js
@@ -3,6 +3,38 @@
 // this is the flie for the title screen
 
 // global variables
-var titleScreen = new Group(); 
 
 
+class titleScreen extends phaser.Screen {
+    
+    // constructor
+    
+    constructor () {
+        super({ key: "titleScene" })
+        this.startButton = null
+    }
+
+Init (data) {
+    this.cameras.main.setBackgroundColor("#000000")
+}
+
+preload () {
+    this.load.image("startButton", "./asset/start.png")
+}
+
+create (data) {
+    console.log("Title Screen")
+    this.startButton = this.add.sprite(400, 300, "startButton").setInteractive()
+    this.startButton.setInteractive({ useHandCursor: true})
+    this.startButton.on("pointerdown", this.startGame, this)
+}
+
+update (time, delta) {
+}
+
+clickButton () {
+    this.scene.start("gameScene")
+}
+}
+
+export default titleScreen
